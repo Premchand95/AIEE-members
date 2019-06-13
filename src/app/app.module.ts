c0fbff3699a2b134bb8282cbfdbae19d5563b005
prem chand <Chandudec10@gmail.com>
2019-06-13 01:08:03 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouteReuseStrategy } from "@angular/router";
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { IonicModule, IonicRouteStrategy } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { HTTP } from "@ionic-native/http/ngx";
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,6 +17,7 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+    HTTP,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { HTTP, HTTPResponse } from "@ionic-native/http/ngx";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
 export class HomePage {
-
-  constructor() {}
-
+  public res: any;
+  private result: number;
+  private page: number;
+  constructor(private http: HTTP) {}
+  //method getList gets all AIEE members from the API
+  public getList(): void {
+    let url = "https://randomuser.me/api/";
+    this.http.setDataSerializer("json");
+    let headers = { Accept: "application/json" };
+    let parameters = {
+      results: this.result.toLocaleString(),
+      pages: this.page.toLocaleString()
+    };
+    this.http.get(url, parameters, headers).then(
+      data => {
+        this.res = JSON.parse(data.data).results; // data received by server
+      },
+      (value: HTTPResponse) => {
+        console.log(value);
+      }
+    );
+    //iterate through Json object
+  }
 }
